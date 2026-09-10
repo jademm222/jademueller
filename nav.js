32a18fa9df6fc851a177a862963da34a207bb9a5
jademm222 <jademm@iastate.edu>
2026-09-10 15:15:51 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
 // Navigation configuration customized for Jade Mueller
 const primaryNavLinks = [
-  { name: "About Me", url: "#about" },
-  { name: "Education", url: "#education" },
-  { name: "Experiences", url: "#experiences" },
-  { name: "Contact Me", url: "#contact" }
+  { name: "About Me", url: "#about.html" },
+  { name: "Education", url: "education.html" },
+  { name: "Experiences", url: "#experiences.html" },
+  { name: "Contact Me", url: "#contact.html" }
 ];
 
 const secondaryNavLinks = [
